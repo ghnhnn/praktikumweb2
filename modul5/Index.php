785fa7da4e8b7fda3399799849ff2cb3b3fd03e5
Ghinaa <101612996+ghnhnn@users.noreply.github.com>
2022-05-15 21:39:25 +0800

    Add files via upload

---
+++ modul5/Index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Perpustakaan</title>
+    <style>
+        table{
+            margin-left:auto;
+            margin-right:auto;
+            border-collapse: collapse;
+        }
+        td{
+            border: 1px solid #000;
+            padding: 5px;
+        }
+    </style>
+</head>
+<body>
+    <h2 style ="text-align:center;"> SISTEM PERPUSTAKAAN</h2>
+    <br></br>
+    <table>
+    <tr>
+            <td><a href="Buku.php">Data Buku</a></td>
+            <td><a href="Member.php">Data Member</a></td>
+            <td><a href="Peminjaman.php">Data Peminjaman</a></td>
+        </tr>
+    </table>
+</body>
+</html>
